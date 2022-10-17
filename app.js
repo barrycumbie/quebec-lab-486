3fb3fb40e091a7499e087f13bf1553647afe95ab
Barry <barrycumbie@gmail.com>
2022-10-17 15:39:38 -0500

    refactor and delete endpoint

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require('express')
 const path = require('node:path')
 const bodyParser = require('body-parser')
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { ObjectId } = require('mongodb')
 const client = new MongoClient(process.env.uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+const PORT = process.env.PORT || 3000;
 console.log(process.env.uri);
 
 const app = express()
@@ -13,42 +14,32 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs');
 
-
 app.get('/', async function (req, res) {
-    client.connect;
-    databasesList = await client.db().admin().listDatabases();
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+  client.connect;
+  client.db("myFirstDatabse").collection("posts").find().toArray().then(result => res.render('index' ,{ players : result  }));
+  // const collection = client.db("myFirstDatabse").collection("posts");
+  // const result = await collection.find().toArray();
+  // console.log(result);
+  // res.render('index' ,{ players : result    })
+  })
 
-    const collection = client.db("myFirstDatabse").collection("posts");
-    console.log('connected!');
-       
-    const result = await collection.find().toArray();
-    
-    console.log(result);
-            
-    //res.send(result);
-
-    res.render('index' ,{ players : result    })
-    
- app.post('/players', (req, res) => {
- console.log(req.body); 
-    client.connect;
-    const collection = client.db("myFirstDatabse").collection("posts");
-    collection.insertOne(req.body)
-    .then(result => {
-      res.redirect('/')
-     })
-    .catch(error => console.error(error))
+app.post('/players', (req, res) => {
+  client.connect;
+  const collection = client.db("myFirstDatabse").collection("posts");
+  collection.insertOne(req.body)
+  .then(result => {
+    res.redirect('/')
+    })
+  .catch(error => console.error(error))
 })
 
-
-          
-});
+app.post('/deletePlayers/:id', (req, res) => {
+  client.connect;
+  client
+    .db("myFirstDatabse").collection("posts").collection.deleteOne(
+      { _id: ObjectId( req.params.id) }).then(result => {
+    res.redirect('/')}).catch(error => console.error(error))
+})
     
-
-
-
-
-app.listen(process.env.PORT || 3000,
-  () => console.log(`server is running on port: ${process.env.PORT}` ));
+app.listen(PORT, 
+  () => console.log(`server is running on port: http://localhost:${ PORT }` ));
